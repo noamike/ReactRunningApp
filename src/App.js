bd51380e34fc2b6c946799a4f684d94fcbeca864
Noah <30814042+noamike@users.noreply.github.com>
2022-03-15 19:18:18 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 export default function App() {
   return (
@@ -8,6 +9,13 @@ export default function App() {
       <table>
         <tr>
           <td>Distance</td>
+          <td>
+            <TextField
+              id="outlined-number"
+              label="number"
+              type="number"
+            ></TextField>
+          </td>
         </tr>
         <tr>
           <td>Notes</td>
